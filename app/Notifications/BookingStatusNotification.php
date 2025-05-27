78ab2bc508a9445cf5f93e8c2eb6b9791fa740cc
elynrose29 <elynrose29@gmail.com>
2025-05-27 14:05:38 -0400

    Update with jules

--- app/Notifications/BookingStatusNotification.php
+++ app/Notifications/BookingStatusNotification.php
@@ -28,14 +28,22 @@ class BookingStatusNotification extends Notification
 
     public function toMail($notifiable): MailMessage
     {
+        $fromDate = \Carbon\Carbon::parse($this->booking->from);
+        $fromTime = \Carbon\Carbon::parse($this->booking->from_time);
+        $toTime = \Carbon\Carbon::parse($this->booking->to_time);
+
         return (new MailMessage)
-            ->subject('Booking Request Update')
-            ->greeting('Hello ' . $notifiable->name)
+            ->subject('Booking Request Update - ' . $this->booking->pet->name)
+            ->greeting('Hello ' . $notifiable->name . ',')
             ->line($this->message)
-            ->line('Pet: ' . $this->booking->pet->name)
-            ->line('Date: ' . $this->booking->from)
-            ->line('Time: ' . $this->booking->from_time . ' - ' . $this->booking->to_time)
-            ->action('View Booking', route('frontend.bookings.show', $this->booking))
-            ->line('Thank you for using our platform!');
+            ->line('Booking Details:')
+            ->line('• Pet: ' . $this->booking->pet->name)
+            ->line('• Date: ' . $fromDate->format('F j, Y'))
+            ->line('• Time: ' . $fromTime->format('g:i A') . ' - ' . $toTime->format('g:i A'))
+            ->line('• Duration: ' . $fromTime->diffInHours($toTime) . ' hours')
+            ->line('• Status: ' . ucfirst($this->booking->status))
+            ->action('View Booking Details', route('frontend.bookings.show', $this->booking))
+            ->line('Thank you for using PetFriends! If you have any questions, please don\'t hesitate to contact us.')
+            ->salutation('Best regards, PetFriends Team');
     }
 } 
